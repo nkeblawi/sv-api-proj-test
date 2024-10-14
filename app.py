0de3d5f0dc617e4d9b9531946aa04217465850c5
nkeblawi <kebnab@gmail.com>
2024-10-14 14:50:36 -0400

    Refactored dataframe and plotting into functions

--- app.py
+++ app.py
@@ -27,27 +27,11 @@ app = Flask(__name__)
 app.static_folder = "static"
 
 
-# Load the index page
-@app.route("/", methods=["GET"])
-def index():
-    return render_template("index.html")
-
-
-# Invoke the API with user inputs and plot the results
-@app.route("/plot", methods=["POST"])  # form action "/plot" method="get" in index.html
-def submit_request():
-    # Get user inputs
-    models = request.form.getlist("model")  # Get a list of selected models
-    selected_date = request.form["date"]
-    date = selected_date.replace("-", "")
-    model_run = request.form["model_run"]
-    tindex = request.form["tindex"]
-    file_suffix = "-forecast.csv"  # hardcoded
-
+# Function to pull data into a dataframe
+def pull_data(models, tindex, model_run, date, file_suffix):
     # Initialize a dictionary to hold data for each model
     model_data = {}
 
-    # Loop through each selected model and make API requests
     for model in models:
         # Build the connection request URL with all the inputs, API key, and headers
         teleconn_URL = f"/{sv_api_version}/model-data/{model}/{date}/{model_run}/teleconnection/{tindex}{file_suffix}?apikey={sv_api_key}"
@@ -56,36 +40,20 @@ def submit_request():
         # Get the response
         res = conn.getresponse()
         data = res.read()
-        print(data)
 
         # Decode the byte data into a string and split into lines
         data_str = data.decode("utf-8")
         df = pd.read_csv(StringIO(data_str), header=None)
-        x_values = df.iloc[:, 0].astype(int).values
 
-        print(x_values)
-
-        y = df.iloc[:, 1:].astype(float).values
-        y_values = y.mean(axis=0).values
-
-        print(y_values)
+        # Get the x and y values; average the y values by hour if the model has ensembles
+        x_values = df.iloc[0, 1:].astype(int).values
+        y = df.iloc[1:, 1:].astype(float).values
+        y_values = y.mean(axis=0)
 
         # Store the data in the model dictionary
         model_data[model] = (x_values, y_values)
 
-    # Plot the data
-    plot_url = plot_data(model_data, tindex, model_run, selected_date)
-
-    return render_template("index.html", plot_url=plot_url, selected_date=selected_date)
-
-
-if __name__ == "__main__":
-    # Debug mode in development environment
-    app.run(debug=False)
-
-    # Production environment
-    # server = WSGIServer(('', 8080), app)
-    # server.serve_forever()
+    return model_data
 
 
 # Function to plot the data:
@@ -112,3 +80,39 @@ def plot_data(model_data, tindex, model_run, date):
     plot_url = base64.b64encode(img.getvalue()).decode("utf8")
 
     return plot_url
+
+
+# Load the index page
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+# Invoke the API with user inputs and plot the results
+@app.route("/plot", methods=["POST"])  # form action "/plot" method="get" in index.html
+def submit_request():
+    # Get user inputs
+    models = request.form.getlist("model")  # Get a list of selected models
+    selected_date = request.form["date"]
+    date = selected_date.replace("-", "")
+    model_run = request.form["model_run"]
+    tindex = request.form["tindex"]
+    file_suffix = "-forecast.csv"  # hardcoded
+
+    # Pull model data
+    model_data = pull_data(models, tindex, model_run, date, file_suffix)
+
+    # Plot the data
+    plot_url = plot_data(model_data, tindex, model_run, date)
+
+    # Display the plotted data
+    return render_template("index.html", plot_url=plot_url, selected_date=selected_date)
+
+
+if __name__ == "__main__":
+    # Debug mode in development environment
+    app.run(debug=False)
+
+    # Production environment
+    # server = WSGIServer(('', 8080), app)
+    # server.serve_forever()
